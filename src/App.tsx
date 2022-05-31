8ee5e91c8201f0aab362ba52693d7c6b09aa13aa
Pronat <Fotolinza@gmail.com>
2022-05-31 23:49:24 +0300

    local storage 7

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ function App() {
 
     const setIncHandler = () => {
         localStorage.setItem('newValue', JSON.stringify(value))
+        localStorage.setItem('newValue1', JSON.stringify(value+1))
     }
 
     const getIncHandler = () => {
@@ -17,12 +18,22 @@ function App() {
         if(val) {
         setValue(JSON.parse(val))   }}
 
+    const remIncHandler = () => {
+        localStorage.removeItem('newValue')
+    }
+
+    const clrIncHandler = () => {
+        localStorage.clear()
+    }
+
         return (
         <div className="App">
             <h1>{value}</h1>
            <button onClick={incHandler}>inc</button>
            <button onClick={setIncHandler}>setInc</button>
            <button onClick={getIncHandler}>getInc</button>
+           <button onClick={remIncHandler}>remInc</button>
+           <button onClick={clrIncHandler}>clrInc</button>
         </div>
     );
 }
