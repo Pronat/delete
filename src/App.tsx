81dc9cb5e1851145d0714e5edf56ad7d3a02aa50
Pronat <Fotolinza@gmail.com>
2022-05-31 20:23:01 +0300

    1

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,38 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+    export const Wrapper  = () => {
+        return <user city="minsk" />
+    }
+    type PropsType = {
+        city: string
+    }
+    export const user: React.FC<PropsType> = (props) => {
+        return <div>hello</div>
+    }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+
+
+
+      {/*<header className="App-header">*/}
+      {/*  <img src={logo} className="App-logo" alt="logo" />*/}
+      {/*  <p>*/}
+      {/*    Edit <code>src/App.tsx</code> and save to reload.*/}
+      {/*  </p>*/}
+      {/*  <a*/}
+      {/*    className="App-link"*/}
+      {/*    href="https://reactjs.org"*/}
+      {/*    target="_blank"*/}
+      {/*    rel="noopener noreferrer"*/}
+      {/*  >*/}
+      {/*    Learn React*/}
+      {/*  </a>*/}
+      {/*</header>*/}
     </div>
   );
 }
