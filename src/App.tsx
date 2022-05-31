9625cd488ea077f36470e7fd86d55b083ef5cb9b
Pronat <Fotolinza@gmail.com>
2022-05-31 23:19:48 +0300

    local storage 5

--- src/App.tsx
+++ src/App.tsx
@@ -3,28 +3,10 @@ import './App.css';
 
 
 function App() {
-    const [value, setValue] = useState<number>(0)
 
-    const incHandler = () => {
-        setValue(value + 1)
-    }
-    const setToLocalStorageHandler = () => {
-        localStorage.setItem('counterValue', JSON.stringify(value))
-    }
-    const getFromLocalStorageHandler = () => {
-        let newValue = localStorage.getItem('counterValue')
-        if (newValue) {
-            let newValueNumber = JSON.parse(newValue)
-            setValue(newValueNumber)
-        }
-
-    }
     return (
         <div className="App">
-            <h1>{value}</h1>
-            <button onClick={incHandler}>inc</button>
-            <button onClick={setToLocalStorageHandler}>setToLocalStorage</button>
-            <button onClick={getFromLocalStorageHandler}>getFromLocalStorage</button>
+
         </div>
     );
 }
