03621f0e4673521a251f77017acabb5761fdcd40
Pronat <Fotolinza@gmail.com>
2022-05-31 23:45:33 +0300

    local storage 6

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,26 @@ import './App.css';
 
 
 function App() {
+    const [value, setValue] = useState<number>(0)
+    const incHandler = () => {
+        setValue(value + 1)
+    }
 
-    return (
+    const setIncHandler = () => {
+        localStorage.setItem('newValue', JSON.stringify(value))
+    }
+
+    const getIncHandler = () => {
+        let val = localStorage.getItem('newValue')
+        if(val) {
+        setValue(JSON.parse(val))   }}
+
+        return (
         <div className="App">
-
+            <h1>{value}</h1>
+           <button onClick={incHandler}>inc</button>
+           <button onClick={setIncHandler}>setInc</button>
+           <button onClick={getIncHandler}>getInc</button>
         </div>
     );
 }
