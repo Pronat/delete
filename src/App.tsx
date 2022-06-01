81dedb3d0bf586a17c3646337310fe369b8966f0
Pronat <Fotolinza@gmail.com>
2022-06-01 21:15:49 +0300

    local storage 10

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,30 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
 
 function App() {
     const [value, setValue] = useState<number>(0)
-    const incHandler = () => {
-        setValue(value + 1)
-        setIncrementHandler()
-    }
-    const setIncrementHandler = () => {
-        localStorage.setItem('keyValue', JSON.stringify(value))
 
-    }
-    const getIncrementHandler = () => {
+    useEffect(()=>{
         let val = localStorage.getItem('keyValue')
         if (val) {
-        setValue(JSON.parse(val))
-    }}
+            setValue(JSON.parse(val))
+        }}, [])
+
+    useEffect(()=>{
+        localStorage.setItem('keyValue', JSON.stringify(value))
+    },[value])
+
+    const incHandler = () => {
+        setValue(value + 1)
+    }
+
+
         return (
         <div className="App">
 
             <h1>{value}</h1>
             <button onClick={incHandler}>inc</button>
-            <button onClick={setIncrementHandler}>setInc</button>
-            <button onClick={getIncrementHandler}>getInc</button>
 
         </div>
     );
