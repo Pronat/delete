9ad3ca213e14c81ca6b36963b7667e4bc1767631
Pronat <Fotolinza@gmail.com>
2022-06-01 23:08:48 +0300

    ekzamen2-2

--- src/ekzamen2/4.tsx
+++ src/ekzamen2/4.tsx
@@ -1,2 +1,20 @@
 import React from 'react';
 
+import React, {useState} from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+function UsersList() {
+    const [users, setUsers] = useState<Array<string>>(["Bob", "Alex", "Ann"])
+
+    return (
+        <p>Тут будет список пользователей</p>
+    )
+}
+
+ReactDOM.render(
+    <UsersList/>, document.getElementById('root')
+);
+// Что вернёт выражение: Array.isArray(users)?
+
+
