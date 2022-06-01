737654c6cc940b8458f940f0fc6bb0b51386ef84
Pronat <Fotolinza@gmail.com>
2022-06-01 20:48:54 +0300

    local storage 8

--- src/App.tsx
+++ src/App.tsx
@@ -4,36 +4,20 @@ import './App.css';
 
 function App() {
     const [value, setValue] = useState<number>(0)
-    const incHandler = () => {
+    const addPlusOneIncrement = () => {
         setValue(value + 1)
     }
+    const setIncrement = () => {
+        localStorage.setItem('keyValue', JSON.stringify(value))
 
-    const setIncHandler = () => {
-        localStorage.setItem('newValue', JSON.stringify(value))
-        localStorage.setItem('newValue1', JSON.stringify(value+1))
     }
-
-    const getIncHandler = () => {
-        let val = localStorage.getItem('newValue')
-        if(val) {
-        setValue(JSON.parse(val))   }}
-
-    const remIncHandler = () => {
-        localStorage.removeItem('newValue')
-    }
-
-    const clrIncHandler = () => {
-        localStorage.clear()
-    }
-
         return (
         <div className="App">
+
             <h1>{value}</h1>
-           <button onClick={incHandler}>inc</button>
-           <button onClick={setIncHandler}>setInc</button>
-           <button onClick={getIncHandler}>getInc</button>
-           <button onClick={remIncHandler}>remInc</button>
-           <button onClick={clrIncHandler}>clrInc</button>
+            <button onClick={addPlusOneIncrement}>inc</button>
+            <button onClick={setIncrement}>setInc</button>
+
         </div>
     );
 }
