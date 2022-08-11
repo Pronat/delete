253dfdff24bdb6130ddd424bc0d361dcd4f55503
AlexPron <Fotolinza@gmail.com>
2022-08-11 21:17:07 +0300

    ekzamen13-3

---
+++ src/ekzamen13/3.tsx
@@ -0,0 +1,47 @@
+import React, {useCallback, useState} from 'react'
+import ReactDOM from 'react-dom'
+
+export const App = () => {
+    const [temp, setTemp] = useState(10)
+    const [seconds, setSeconds] = useState(100)
+
+    const increaseSeconds = () => setSeconds(seconds + 10)
+    // useEffect(() => setCounter(count + 1), [count])
+    // const increaseTemp = XXX
+
+
+    return <>
+        <TempDisplay temp={temp} increaseTemp={increaseTemp}/>
+
+        <div>
+            <b>Секунды :</b> {seconds} с
+            <button style={{marginLeft: '15px'}}
+                    onClick={increaseSeconds}>
+                Увеличить на 10 секунд
+            </button>
+        </div>
+    </>
+}
+const TempDisplay = React.memo((props: any) => {
+    console.log('Render TempDisplay')
+    return (
+        <div style={{marginBottom: '15px'}}
+             onClick={props.reset}>
+            <b>Температура:</b> {props.temp} &#176;
+            <button style={{marginLeft: '15px'}}
+                    onClick={props.increaseTemp}>
+                Увеличить температуру на 1 градус
+            </button>
+        </div>
+    )
+})
+
+ReactDOM.render(<App/>, document.getElementById('root'));
+
+// Что надо написать вместо XXX для того, чтобы обязательно выполнялись 2 условия:
+// 1) При нажатии на кнопку "Увеличить температуру на 1 градус" температура увеличивалась
+// 2) Компонент TempDisplay не должен перерисовываться при нажатии на кнопку "Увеличить на 10 секунд"
+
+// Пример ответа: useEffect(() => setCounter(count + 1), [count])
+
+// неправильно const increaseTemp = useCallback(() => setTemp(temp + 1),[setTemp])
